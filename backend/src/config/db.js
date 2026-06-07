d2e13d40df68ccd72f94b112f2f57863fd374185
Vanishukla97 <vanishukla2006@gmail.com>
2026-06-07 17:40:40 +0530

    Add SSL config for TiDB Cloud connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   user: env.db.user,
   password: env.db.password,
   database: env.db.name,
+  ssl: {},
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
